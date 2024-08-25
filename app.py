d5f68eff3801438c668c9a8414b1a626f7d78cfa
Murtaza824 <66223556+Murtaza824@users.noreply.github.com>
2024-08-25 18:44:53 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.header('Tossing a Coin')
+
+number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
+start_button = st.button('Run')
+
+if start_button:
+    st.write(f'Running the experiment of {number_of_trials} trials')
